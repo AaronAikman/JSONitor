2b76ab1e88eb897331c0717ccd3130d7705b1b1f
Aaron <gamephreakus@yahoo.com>
2018-07-28 09:19:25 -0700

    folding, tab navigation, and some cleanup

--- source/JSONitor.py
+++ source/JSONitor.py
@@ -10,7 +10,6 @@ logger
 save temp files/autosave
 Use json file for ui colors/settings
 Add goto line
-source folding
 bookmarking
 find and replace
 conversion to (xml, csv, yaml)
@@ -116,8 +115,11 @@ class MyWindow(QMainWindow):
         self.actionSave.triggered.connect(self.saveFile)
         self.actionSave_As.triggered.connect(self.saveAs)
         self.actionNew.triggered.connect(self.newFile)
-        self.actionClose.triggered.connect(self.closeTab)
-        self.actionReopen_Tab.triggered.connect(self.reopenTab)
+        self.actionClose.triggered.connect(self.onTabClose)
+        self.actionReopen_Tab.triggered.connect(self.onTabReopen)
+        self.actionCycle_Tabs.triggered.connect(self.onTabCycle)
+        self.actionPrevious_Tab.triggered.connect(self.onTabPrev)
+        self.actionNext_Tab.triggered.connect(self.onTabNext)
         self.actionQuit.triggered.connect(self.closeWindow)
         # print(dir(self.filepathLineEdit))
         self.filepathLineEdit.returnPressed.connect(self.lineEditEnter)
@@ -162,32 +164,9 @@ class MyWindow(QMainWindow):
         self.tabLayoutList = []
         self.tabs = QTabWidget()
         self.tabs.currentChanged.connect(self.onTabChange)
-        # self.tab1 = QWidget()
-        # self.tab2 = QWidget()
-        # self.tabs.resize(300,200)
-
-        # # Add tabs
-        # self.tabs.addTab(self.tab1,"Tab 1")
-        # self.tabs.addTab(self.tab2,"Tab 2")
-
-        # # Create first tab
-        # self.tab1.layout = QVBoxLayout(self)
-        # self.tabLayoutList.append(self.tab1.layout)
-        # # self.pushButton1 = QPushButton("PyQt5 button")
-        # self.tab1.layout.addWidget(self.__editor)
-        # self.tab1.setLayout(self.tab1.layout)
-
-        # # Create second tab
-        # self.tab2.layout = QVBoxLayout(self)
-        # self.tabLayoutList.append(self.tab2.layout)
-        # # self.tab2.layout.addWidget(self.__editor)
-        # self.tab2.setLayout(self.tab2.layout)
 
         # Add tabs to widget
         self.gridLayout.addWidget(self.tabs)
-        # self.setLayout(self.layout)
-
-        # self.gridLayout.addWidget(self.__editor)
 
         # Deleting Text Edit until I decide if I want it back
         self.gridLayout.removeWidget(self.textEdit)
@@ -205,13 +184,15 @@ class MyWindow(QMainWindow):
         self.statusbar.addWidget(self.lineColLabel)
 
 
-        # self.next_item_is_table = False
+        # Info
         self.pages = []
         self.tEditors = []
         self.lineEdits = []
         self.files = []
 
+        # History
         self.recentlyClosedFiles = []
+        self.recentlyAccessedTabs = []
         self.add_page()
 
 
@@ -227,42 +208,37 @@ class MyWindow(QMainWindow):
         return lineEdit
 
     def create_textEditor(self):
-        # list = QListWidget()
-    #     columns = random.randint(2,5)
-    #     for c in range(columns):
-    #         QListWidgetItem( str( random.randint(0,10) ), list )
 
-    #     return list
 
         # ! Make instance of QSciScintilla class!
         # ----------------------------------------
         tEditor = QsciScintilla()
-    #     tEditor.setText('''{
-    # "glossary": {
-    #     "title": "example glossary",
-    #     "GlossDiv": {
-    #     "title": "S",
-    #     "GlossList": {
-    #         "GlossEntry": {
-    #         "ID": "SGML",
-    #         "SortAs": "SGML",
-    #         "GlossTerm": "Standard Generalized Markup Language",
-    #         "Acronym": "SGML",
-    #         "Abbrev": "ISO 8879:1986",
-    #         "GlossDef": {
-    #             "para": "A meta-markup language, used to create markup languages such as DocBook.",
-    #             "GlossSeeAlso": [
-    #             "GML",
-    #             "XML"
-    #             ]
-    #         },
-    #         "GlossSee": "markup"
-    #         }
-    #     }
-    #     }
-    # }
-    # }
-    # ''')
+        tEditor.setText('''{
+    "glossary": {
+        "title": "example glossary",
+        "GlossDiv": {
+        "title": "S",
+        "GlossList": {
+            "GlossEntry": {
+            "ID": "SGML",
+            "SortAs": "SGML",
+            "GlossTerm": "Standard Generalized Markup Language",
+            "Acronym": "SGML",
+            "Abbrev": "ISO 8879:1986",
+            "GlossDef": {
+                "para": "A meta-markup language, used to create markup languages such as DocBook.",
+                "GlossSeeAlso": [
+                "GML",
+                "XML"
+                ]
+            },
+            "GlossSee": "markup"
+            }
+        }
+        }
+    }
+    }
+    ''')
         tEditor.setUtf8(True)             # Set encoding to UTF-8
         tEditor.setFont(self.__myFont)
 
@@ -306,6 +282,10 @@ class MyWindow(QMainWindow):
         # self.__lexer.setColor(Qt.gray)
         # self.__lexer.setPaper(Qt.gray)
         self.__lexer.setDefaultFont(self.__myFont)
+        # self.__lexer.setFolding(QsciScintilla.BoxedTreeFoldStyle)
+        tEditor.setFolding(True)
+        # print(help(self.__lexer.setProperty))
+        # self.__lexer.setFoldCompact(True)
 
         # self.__lexer = QsciLexerXML(tEditor)
         # self.__lexer = QsciLexerYAML(tEditor)
@@ -323,6 +303,27 @@ class MyWindow(QMainWindow):
 
         tEditor.textChanged.connect(self.asteriskTitle)
         tEditor.cursorPositionChanged.connect(self.updateLineColInfo)
+
+
+        # Drops
+        tEditor.setAcceptDrops(True)
+
+
+        # Multiline Editing
+        # print(help(tEditor.cursorPositionChanged))
+        tEditor.SendScintilla(tEditor.SCI_SETADDITIONALSELECTIONTYPING, 1)
+        # offset =tEditor.positionFromLineIndex(0, 7) # line-index to offset
+        # tEditor.SendScintilla(tEditor.SCI_SETSELECTION, offset, offset)
+        # # using the same offset twice selects no characters, hence a cursor
+
+        # offset = tEditor.positionFromLineIndex(1, 5)
+        # tEditor.SendScintilla(tEditor.SCI_ADDSELECTION, offset, offset)
+
+        # offset = tEditor.positionFromLineIndex(2, 5)
+        # tEditor.SendScintilla(tEditor.SCI_ADDSELECTION, offset, offset)
+
+
+
 
         self.tEditors.append(tEditor)
 
@@ -452,18 +453,26 @@ class MyWindow(QMainWindow):
         else:
             self.saveAs()
 
+
     def saveAs(self):
         self.saveFile(1)
+
 
     def quickPrompt(self, title, message):
         reply = QMessageBox.question(self, title,
                         message, QMessageBox.Yes, QMessageBox.No)
         return reply == QMessageBox.Yes
 
+
     def closeWindow(self):
         sys.exit()
 
-    def closeTab(self):
+
+    def onTabClose(self):
+        if self.tabs.tabText(self.tabs.currentIndex())[-1] == '*':
+            if self.quickPrompt('Save?', 'Do you want to save before closing the tab?'):
+                self.saveFile()
+
         tabIndex = self.tabs.currentIndex()
         self.recentlyClosedFiles.append(self.files[self.tabs.currentIndex()])
         self.tabs.removeTab(tabIndex)
@@ -472,15 +481,14 @@ class MyWindow(QMainWindow):
         del self.tEditors[tabIndex]
         del self.lineEdits[tabIndex]
         del self.files[tabIndex]
-        # TODO also remove index
 
-    def reopenTab(self):
+
+    def onTabReopen(self):
         reopening = True
         while reopening:
             if self.recentlyClosedFiles:
                 if os.path.isfile(self.recentlyClosedFiles[-1]):
                     self.currentFile = self.recentlyClosedFiles[-1]
-                    # self.recentlyClosedFiles = self.recentlyClosedFiles[:-1]
                     del self.recentlyClosedFiles[-1]
                     self.openFile()
                     reopening = False
@@ -489,30 +497,24 @@ class MyWindow(QMainWindow):
             else:
                 reopening = False
 
+
     def asteriskTitle(self):
         tabName = '{}*'.format(os.path.splitext(os.path.basename(self.lineEdits[self.tabs.currentIndex()].text()))[0])
         self.tabs.setTabText(self.tabs.currentIndex(), tabName)
         self.setWindowTitle('{} - {}'.format(self.title, tabName))
-        # if self.currentFile:
+
 
     def updateLineColInfo(self):
         cursorLine, cursorCol = self.tEditors[self.tabs.currentIndex()].getCursorPosition()
-        # print(self.lineColLabel)
         self.lineColLabel.setText('Ln {}, Cl {}'.format(cursorLine + 1, cursorCol + 1))
 
 
     def onTabChange(self):
-        # tabName = '{}'.format(os.path.splitext(os.path.basename(self.lineEdits[-1].text()))[0])
-        # self.tabs.setTabText(self.tabs.currentIndex(), tabName)
         self.setWindowTitle('{} - {}'.format(self.title, self.tabs.tabText(self.tabs.currentIndex())))
-        # print(self.files)
-        # print(dir(self.tabs))
-        # print(self.tabs.currentIndex())
-        # print(self.tabLayoutList)
-        # ind = self.tabs.currentIndex()
-        # self.tabLayoutList[ind].addWidget(self.__editor)
-        # print(self.tabs.currentWidget().layout.addWidget(self.__editor))
-        # pass
+        self.recentlyAccessedTabs.append(self.tabs.currentIndex())
+        if len(self.recentlyAccessedTabs) > 2:
+            del self.recentlyAccessedTabs[0]
+
 
     def create_page(self, *contents):
         page = QWidget()
@@ -523,53 +525,36 @@ class MyWindow(QMainWindow):
         page.setLayout(vbox)
         return page
 
-    # def create_table(self):
-    #     rows, columns = random.randint(2,5), random.randint(1,5)
-    #     table = QTableWidget( rows, columns )
-    #     for r in range(rows):
-    #         for c in range(columns):
-    #             table.setItem( r, c, QTableWidgetItem( str( random.randint(0,10) ) ) )
-    #     return table
-
-    # def create_list(self):
-    #     list = QListWidget()
-    #     columns = random.randint(2,5)
-    #     for c in range(columns):
-    #         QListWidgetItem( str( random.randint(0,10) ), list )
-
-    #     return list
 
     def create_new_page_button(self):
         btn = QPushButton('Create a new page!')
         btn.clicked.connect(self.add_page)
         return btn
 
+
     def add_page(self):
         self.pages.append( self.create_page( self.createLineEdit(), self.create_textEditor()) )
-        # if self.next_item_is_table:
-            # self.pages.append( self.create_page( self.create_table(), self.create_new_page_button() ) )
-            # self.next_item_is_table = False
-        # else:
-        #     self.pages.append( self.create_page( self.create_list(), self.create_new_page_button() ) )
-        #     self.next_item_is_table = True
-        # print(os.path.split(self.currentFile)[-1])
-        # inputFilepath = self.currentFile
         tabName = os.path.splitext(os.path.basename(self.lineEdits[-1].text()))[0]
-        # TODO add to open and save tabs as well
-        # filename_w_ext = os.path.basename(inputFilepath)
-        # filename, file_extension = os.path.splitext(filename_w_ext)
-        # print(filename)
         self.tabs.addTab( self.pages[-1] , tabName )
         self.tabs.setCurrentIndex( len(self.pages)-1 )
-        # print(dir(self.pages[0]))
-        # self.pages[-1].setTabText(os.path.dirname(self.currentFile))
-        # print(dir(self.tabs.currentWidget()))
-        # self.tabs.currentWidget().setWindowTitle('hi')
-
 
 
     def onTabCycle(self):
-        self.tabs.setCurrentIndex(self.tabs.currentIndex()+1)
+        tabInd = self.recentlyAccessedTabs[-2]
+        self.tabs.setCurrentIndex(tabInd)
+
+
+    def onTabPrev(self):
+        tabInd = self.tabs.currentIndex()
+        tabInd = (tabInd - 1) if tabInd > 0 else (len(self.pages) - 1)
+        self.tabs.setCurrentIndex(tabInd)
+
+
+    def onTabNext(self):
+        tabInd = self.tabs.currentIndex()
+        tabInd = (tabInd + 1) if tabInd < (len(self.pages) - 1) else 0
+        self.tabs.setCurrentIndex(tabInd)
+
 
     def lineEditEnter(self):
         # TODO make sure you can't lose your changes to current file
@@ -591,16 +576,7 @@ class MyWindow(QMainWindow):
 
 
     def newFile(self):
-        # if self.quickPrompt('Save?', 'Do you want to save before proceeding?'):
-        #     self.saveFile()
-        # self.textEdit.clear()
-        # self.filepathLineEdit.clear()
-        # self.currentFile = None
         self.add_page()
-
-
-
-
 
 
 if __name__ == '__main__':
